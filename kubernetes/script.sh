f87d7811bb6280e84ec80958e3927644ed28a39e
Thiago Paz <thiagop4z@gmail.com>
2021-10-05 04:39:03 -0300

    Final kubernetes solution

--- kubernetes/script.sh
+++ kubernetes/script.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/thiagop4z/desafios-devops/archive/refs/heads/master.zip && unzip master.zip &&
-cd desafios-devops-master/kubernetes/ && eval $(minikube docker-env) && docker build -t idwall:1.0 . &&
-kubectl apply -f idwall.yaml && sudo -- sh -c "echo '$(minikube ip) idwall.cc' >> /etc/hosts" &&
-sleep 10s && curl idwall.cc:31000
-
-#sudo apt install unzip -y \
-
-#curl $(minikube service --url idwall)
+wget https://github.com/thiagop4z/desafios-devops/archive/refs/heads/master.zip && unzip master.zip && cd desafios-devops-master/kubernetes/ && eval $(minikube docker-env) && docker build -t idwall:1.0 . && helm install idwall . && sudo -- sh -c "echo '$(minikube ip) idwall.cc' >> /etc/hosts" && sleep 10s && curl idwall.cc
